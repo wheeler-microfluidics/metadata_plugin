e6562927f5765c0a0789b36c92e27319846f545d
Christian Fobel <christian@fobel.net>
2016-08-11 15:36:29 -0400

    [NB] Update hook scripts (1.1.post12)
    Update hook scripts using [`microdrop-plugin-template==1.1.post12`][1].
    
    [1]: https://github.com/wheeler-microfluidics/microdrop-plugin-template/

--- on_plugin_install.py
+++ on_plugin_install.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 import logging
+import sys
 
 from path_helpers import path
 from pip_helpers import install
 
 
-if __name__ == '__main__':
+def main(ostream=sys.stdout):
     logging.basicConfig(level=logging.INFO)
     plugin_root = path(__file__).parent.abspath()
     logging.info('[%s] Processing post-install hook for: %s',
@@ -14,10 +15,12 @@ if __name__ == '__main__':
     if requirements_file.exists():
         # Install required packages using `pip`, with Wheeler Lab wheels server
         # for binary wheels not available on `PyPi`.
-        print install(['--find-links', 'http://192.99.4.95/wheels',
-                       '--trusted-host', '192.99.4.95', '-r',
-                       requirements_file])
-        print ('[%s] Completed post-install processing for: %s' %
-               (str(datetime.now()), plugin_root.name))
-        print 'Press <Enter> key to continue...'
-        raw_input()
+        print >> ostream, install(['--find-links', 'http://192.99.4.95/wheels',
+                                   '--trusted-host', '192.99.4.95', '-r',
+                                   requirements_file])
+        print >> ostream, ('[%s] Completed post-install processing for: %s' %
+                           (str(datetime.now()), plugin_root.name))
+
+
+if __name__ == '__main__':
+    main()
